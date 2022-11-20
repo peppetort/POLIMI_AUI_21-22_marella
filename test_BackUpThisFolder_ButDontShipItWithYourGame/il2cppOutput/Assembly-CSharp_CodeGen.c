0c4db548762e4e9d2daf70cf0a7914d8982c6f11
LucaMong <luca.andrulli@gmail.com>
2022-11-20 21:39:00 +0100

    Spero Vada

---
+++ test_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ExitManager::Update()
+extern void ExitManager_Update_m60069D8FE5FDD29EDEBD0868DD5317E270FC0227 (void);
+// 0x00000002 System.Void ExitManager::.ctor()
+extern void ExitManager__ctor_m445B996C3352BAEE016F5CC3B6E9634C2ADF05D0 (void);
+// 0x00000003 System.Void MainMenu::PlayGame()
+extern void MainMenu_PlayGame_mEC2DA4274CDD4D658AE27480E2C70C6049A496B5 (void);
+// 0x00000004 System.Void MainMenu::PlayStories()
+extern void MainMenu_PlayStories_m22BB87BEA4C81EEB0C8E9B408038396EFF3C0868 (void);
+// 0x00000005 System.Void MainMenu::.ctor()
+extern void MainMenu__ctor_m8209CEC1D907C87A96D777961F4D0536E6E948DD (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ExitManager_Update_m60069D8FE5FDD29EDEBD0868DD5317E270FC0227,
+	ExitManager__ctor_m445B996C3352BAEE016F5CC3B6E9634C2ADF05D0,
+	MainMenu_PlayGame_mEC2DA4274CDD4D658AE27480E2C70C6049A496B5,
+	MainMenu_PlayStories_m22BB87BEA4C81EEB0C8E9B408038396EFF3C0868,
+	MainMenu__ctor_m8209CEC1D907C87A96D777961F4D0536E6E948DD,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	5448,
+	5448,
+	5448,
+	5448,
+	5448,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
